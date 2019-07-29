bee4733d8e62d8fe7fa1590c848df35a32f99140
William Grigor <bjgrigor@gmail.com>
2019-07-29 18:24:40 -0500

    Added confirmation message to autorole function

--- main.py
+++ main.py
@@ -11,131 +11,146 @@ bot = commands.Bot(command_prefix="b!")
 
 @bot.event
 async def on_ready():
-	print("Ready")
-	config = json.loads(open('assets/config.json', 'r').read())
-	# Check if there are any new servers the bot does not have configs for
-	for server in bot.guilds:
-		if str(server.id) not in config:
-			# Add empty config to JSON + initialize all win/loss stats for users
-			config[server.id] = {
-									"verification_channel": None,
-									"verification_role": None,
-									"reporting_channel": None,
-									"reports": {}
-								}
-			# Save to config file
-			json.dump(config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
+    print("Ready")
+    config = json.loads(open('assets/config.json', 'r').read())
+    # Check if there are any new servers the bot does not have configs for
+    for server in bot.guilds:
+        if str(server.id) not in config:
+            # Add empty config to JSON + initialize all win/loss stats for users
+            config[server.id] = {
+                "verification_channel": None,
+                "verification_role": None,
+                "reporting_channel": None,
+                "reports": {}
+            }
+            # Save to config file
+            json.dump(config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
 
 
 @bot.event
 async def on_message(message):
-	if message.guild is None:
-		return await bot.process_commands(message)
-	elif message.author == bot.user:
-		return
+    if message.guild is None:
+        return await bot.process_commands(message)
+    elif message.author == bot.user:
+        return
 
-	config_full = json.loads(open('assets/config.json', 'r').read())
-	config = config_full[str(message.guild.id)]
-	verification_enabled = True if config["verification_channel"] is not None else False
-	if verification_enabled:
-		# Check if the user is attempting to verify, if not then delete the message and send them a notice in DM
-		verify_channel = config['verification_channel']
-		unverified_role = get(message.author.guild.roles, name="Unverified")
-		if unverified_role in message.author.roles and (message.channel.id != verify_channel and message.content != "b!verify"):
-			await message.channel.purge(limit=1)
-			await message.author.send(
-				"You have not verified your account, please type 'b!verify' in your server's verification channel")
+    config_full = json.loads(open('assets/config.json', 'r').read())
+    config = config_full[str(message.guild.id)]
+    verification_enabled = True if config["verification_channel"] is not None else False
+    if verification_enabled:
+        # Check if the user is attempting to verify, if not then delete the message and send them a notice in DM
+        verify_channel = config['verification_channel']
+        unverified_role = get(message.author.guild.roles, name="Unverified")
+        if unverified_role in message.author.roles and (
+                message.channel.id != verify_channel and message.content != "b!verify"):
+            await message.channel.purge(limit=1)
+            await message.author.send(
+                "You have not verified your account, please type 'b!verify' in your server's verification channel")
 
-	if str(message.channel) == 'if-you-are-new-click-here' and message.content is not None:
-		word_set = set(message.content.split())
-		roles = {role.name:role for role in message.guild.roles if role.name != '@everyone' and role.name !='Admin' and role.name != 'Moderator'}
+    if str(message.channel) == 'if-you-are-new-click-here' and message.content is not None:
+        word_set = set(message.content.split())
 
-		member_roles = [roles.get(word, 0) for word in word_set if roles.get(word,0)!=0]
-		await message.author.add_roles(*member_roles)
+        ignored_roles = ['@everyone', 'Admin', 'Moderator',
+                         'Merit Badge (lvl - Moderator)',
+                         'Merit Badge (lvl - Admin)',
+                         'Merit Badge (lvl - Owner)']
 
-	await bot.process_commands(message)
+        roles = {role.name: role for role in message.guild.roles if role.name not in ignored_roles}
+        member_roles = [roles.get(word, 0) for word in word_set if roles.get(word, 0) != 0]
+        await message.author.add_roles(*member_roles)
+
+        newline = '\n'
+        await message.author.send(
+            f'Hello, based on your introduction, you have automatically been assigned the following roles: \n'
+            f'{newline.join([role.name for role in member_roles])} \n'
+            'If you believe you are missing some roles or have received roles that do not apply to you, '
+            'please feel free to contact the moderation team'
+        )
+
+    await bot.process_commands(message)
 
 
 @bot.event
 async def on_member_join(member):
-	config_full = json.loads(open('assets/config.json', 'r').read())
-	config = config_full[str(member.guild.id)]
-	verification_enabled = True if config["verification_channel"] is not None else False
-	if verification_enabled and not member.bot:
-		role = get(member.guild.roles, id=config["verification_role"])
-		await member.add_roles(role)
+    config_full = json.loads(open('assets/config.json', 'r').read())
+    config = config_full[str(member.guild.id)]
+    verification_enabled = True if config["verification_channel"] is not None else False
+    if verification_enabled and not member.bot:
+        role = get(member.guild.roles, id=config["verification_role"])
+        await member.add_roles(role)
 
-	message = open('assets/welcome_message.txt').read()
-	await member.send(message)
+    message = open('assets/welcome_message.txt').read()
+    await member.send(message)
 
-	# Prepare welcome embed
-	embed = Embed(
-					color=0x9370DB,
-					description=f'Welcome to the server! You are member number {len(list(member.guild.members))}'
-				)
-	embed.set_thumbnail(url=member.avatar_url)
-	embed.set_author(name=member.name, icon_url=member.avatar_url)
-	embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
-	embed.timestamp = datetime.utcnow()
+    # Prepare welcome embed
+    embed = Embed(
+        color=0x9370DB,
+        description=f'Welcome to the server! You are member number {len(list(member.guild.members))}'
+    )
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_author(name=member.name, icon_url=member.avatar_url)
+    embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
+    embed.timestamp = datetime.utcnow()
 
-	# Get the server message channel and send welcome message there
-	channel = bot.get_channel(id=member.guild.system_channel.id)
+    # Get the server message channel and send welcome message there
+    channel = bot.get_channel(id=member.guild.system_channel.id)
 
-	await channel.send(embed=embed)
+    await channel.send(embed=embed)
 
 
 @bot.event
 async def on_member_remove(member):
-	# Prepare goodbye embed
-	embed = Embed(color=0x9370DB, description=f'Goodbye! Thank you for spending time with us!')
-	embed.set_thumbnail(url=member.avatar_url)
-	embed.set_author(name=member.name, icon_url=member.avatar_url)
-	embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
-	embed.timestamp = datetime.utcnow()
+    # Prepare goodbye embed
+    embed = Embed(color=0x9370DB, description=f'Goodbye! Thank you for spending time with us!')
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_author(name=member.name, icon_url=member.avatar_url)
+    embed.set_footer(text=member.guild, icon_url=member.guild.icon_url)
+    embed.timestamp = datetime.utcnow()
 
-	# Get the server message channel and send goodbye message there
-	channel = bot.get_channel(id=member.guild.system_channel.id)
+    # Get the server message channel and send goodbye message there
+    channel = bot.get_channel(id=member.guild.system_channel.id)
 
-	await channel.send(embed=embed)
+    await channel.send(embed=embed)
 
 
 @bot.event
 async def on_guild_join(guild):
-	config = json.loads(open('assets/config.json', 'r').read())
-	# Create configuration dict to store in JSON
-	config[str(guild.id)] = {
-		"verification_channel": None,
-		"verification_role": None,
-		"reporting_channel": None,
-		"reports": {}
-	}
-	# Save to config file
-	json.dump(config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
+    config = json.loads(open('assets/config.json', 'r').read())
+    # Create configuration dict to store in JSON
+    config[str(guild.id)] = {
+        "verification_channel": None,
+        "verification_role": None,
+        "reporting_channel": None,
+        "reports": {}
+    }
+    # Save to config file
+    json.dump(config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
 
 
 @bot.event
 async def on_guild_remove(guild):
-	config = json.loads(open('assets/config.json', 'r').read())
-	config.pop(str(guild.id))
-	json.dump(config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
+    config = json.loads(open('assets/config.json', 'r').read())
+    config.pop(str(guild.id))
+    json.dump(config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
 
 
 if __name__ == '__main__':
-	token = None
-	try:
-		# Attempt to fetch the token from config.json
-		token = json.loads(open('assets/config.json', 'r').read())['token']
-	except FileNotFoundError:
-		# If config.json does not exist, it must be the first time starting the bot, run through configuration
-		# If we are running from a docker container, fetch the token through an environment variable, otherwise, prompt the user to enter it
-		environment = os.environ.get('BOT_TOKEN', None)
-		token = environment if environment is not None else input('It appears this is the first time running the bot. Please enter your bot\'s token: ')
-		initial_config = {"token": token}
-		json.dump(initial_config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
-		os.mkdir('./assets/network_charts')
-		os.mkdir('./assets/role_charts')
-	finally:
-		for file in os.listdir('./cogs'):
-			if file.endswith('.py'):
-				bot.load_extension(f'cogs.{file[:-3]}')
-		bot.run(token)
+    token = None
+    try:
+        # Attempt to fetch the token from config.json
+        token = json.loads(open('assets/config.json', 'r').read())['token']
+    except FileNotFoundError:
+        # If config.json does not exist, it must be the first time starting the bot, run through configuration
+        # If we are running from a docker container, fetch the token through an environment variable, otherwise, prompt the user to enter it
+        environment = os.environ.get('BOT_TOKEN', None)
+        token = environment if environment is not None else input(
+            'It appears this is the first time running the bot. Please enter your bot\'s token: ')
+        initial_config = {"token": token}
+        json.dump(initial_config, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
+        os.mkdir('./assets/network_charts')
+        os.mkdir('./assets/role_charts')
+    finally:
+        for file in os.listdir('./cogs'):
+            if file.endswith('.py'):
+                bot.load_extension(f'cogs.{file[:-3]}')
+        bot.run(token)
