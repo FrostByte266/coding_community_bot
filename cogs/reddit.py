ec3808135a5be56f82827d90fbdde028de41fe5e
DPR-Sanchez <david.p.sanchez@enmu.edu>
2019-08-06 20:24:38 -0600

    removed shuffle()

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -43,7 +43,7 @@ async def readings_fetch(ctx,subreddits_list, period='year', mode='top'):
     for subreddit in subreddits_list:
         top_links_in_period.extend(topinxperiod(subreddit, period, links_per_sub))
 
-    shuffle(top_links_in_period)
+    
     await ctx.send('composite: ' + str(len(top_links_in_period)))
     top_links_in_period = sample(top_links_in_period,5)
 
