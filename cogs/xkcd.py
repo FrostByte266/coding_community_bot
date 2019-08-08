961ddb3747b667a097f6e7a0fe13d3233b231322
ephreal <tjannx@hotmail.com>
2019-08-07 20:27:00 -0700

    Added xkcd comic grabber

---
+++ cogs/xkcd.py
@@ -0,0 +1,59 @@
+import aiohttp
+import json
+import random
+
+from discord.ext import commands
+
+class XkcdFetcher(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.prefix = self.bot.command_prefix
+
+	async def get_quote(self, url):
+		async with aiohttp.ClientSession() as session:
+			html = await self.fetch(session, url)
+			return html
+
+
+	async def fetch(self, session, url):
+		async with session.get(url) as html:
+			return await html.text()
+
+
+	@commands.command(description="gets an XKCD webcomic")
+	async def xkcd(self, ctx):
+		"""
+		Gets an XKCD comic. By default, will grab a random comic.
+
+		Examples:
+			Get a random webcomic
+			{self.prefix}xckd
+
+			Get comic number 404
+			{self.prefix}xkcd 404
+		"""
+
+		command = ctx.message.content.split(" ")
+		command = command[1:]
+
+		latest = await self.get_quote("https://xkcd.com/info.0.json")
+		latest = json.loads(latest)
+
+		if command:
+			try:
+				xkcd_num = int(command[0])
+				if xkcd_num > latest["num"]:
+					xkcd_num = latest["num"]
+			except ValueError:
+				await ctx.send("I don't know what to do with that. I'll give "
+							   "you a random comic for now. If you're "
+							   "unsure of how to use this command, run\n"
+							   "```.help xkcd```")
+				xkcd_num = random.randint(1, latest["num"])
+		else:
+			xkcd_num = random.randint(1, latest["num"])
+
+		await ctx.send(f"https://xkcd.com/{xkcd_num}")
+
+def setup(bot):
+	bot.add_cog(XkcdFetcher(bot))
