34fbe397993162997d669d2d64447dacdd1a9932
DPR-Sanchez <david.p.sanchez@enmu.edu>
2020-01-02 21:38:00 -0700

    switched print to ctx.send

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -153,7 +153,7 @@ class Reddit(commands.Cog):
 
             # category is a list of subreddit names to be concatenated after r/
             category = sample(Reddit.reddit.sub_reddit_composite, 5)
-            print(readings_fetch(category, period, mode))
+            ctx.send(readings_fetch(category, period, mode))
 
         except Exception as e:
             print(e)
