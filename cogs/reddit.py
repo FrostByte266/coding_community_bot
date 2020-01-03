0cbf48e3e88b4992aea9b958a39be93fef315170
DPR-Sanchez <david.p.sanchez@enmu.edu>
2020-01-02 20:01:12 -0700

    praw feature polishing

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,36 +1,33 @@
-from bs4 import BeautifulSoup
 from discord.ext import commands
-from random import sample, shuffle
+from random import sample
 
 import praw
 
-import requests
-
-headers = {
-    'User-Agent': '',
-    'Accept': '',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Referer': '',
-    'Connection': '',
-    'Upgrade-Insecure-Requests': '1',
-    'Cache-Control': 'max-age=0',
-}
-
+async def reddit_bot():
+    try:
+        reddit_bot.init
+    except Exception as e:
+        reddit_bot.init = True
+        reddit_bot.reddit = praw.Reddit(
+            client_id='O59DAVYqBJvTtA',
+            client_secret='placeholder',
+            user_agent='placeholder',
+            username='placeholder',
+            password='placeholder'
+        )
 
 async def topinxperiod(subreddit, period='year', return_quantity=3):
-    if return_quantity > 7:
-        return_quantity = 7
 
-    # SQnoC3ObvgnGjWt90zD9Z is the div class on reddit that containts the center panes list
+    try:
+        if return_quantity > 7:
+            return_quantity = 7
 
-    reddit = praw.Reddit(
-                            client_id='my client id',
-                            client_secret='my client secret',
-                            user_agent='my user agent'
-                        )
-
-    # relevant documentation https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html
-    return [submission.url for submission in reddit.subreddit(subreddit[0]).top(period,limit=return_quantity)]
+        reddit = reddit_bot.reddit
+        #print(reddit.read_only)
+        # relevant documentation https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html
+        return [submission.url for submission in reddit.subreddit(subreddit).top(period,limit=return_quantity)]
+    except Exception as e:
+        return str(e)
 
 
 async def readings_fetch(ctx, subreddits_list, period='year', mode='top'):
@@ -41,13 +38,15 @@ async def readings_fetch(ctx, subreddits_list, period='year', mode='top'):
     else:
         links_per_sub = 5
 
-    await ctx.send(str(subreddits_list))
-    for subreddit in subreddits_list:
-        top_links_in_period.extend(await topinxperiod(subreddit, period, links_per_sub))
+    print(str(subreddits_list))
+    try:
+        for subreddit in subreddits_list:
+            top_links_in_period.extend(topinxperiod(subreddit, period, links_per_sub))
+    except Exception as e:
+        print(e)
 
-    await ctx.send('top links: ' + str(len(top_links_in_period)))
-    top_links_in_period = sample(top_links_in_period,5)
-
+    print('\ntop links: ' + str(len(top_links_in_period)))
+    top_links_in_period = sample(top_links_in_period, 5)
 
     while len('\n'.join([str(x) for x in top_links_in_period])) > 2000:
         top_links_in_period.pop(-1)
@@ -55,7 +54,7 @@ async def readings_fetch(ctx, subreddits_list, period='year', mode='top'):
     return '\n'.join([str(x) for x in top_links_in_period])
 
 
-def test_top_readings(list_of_lists):
+async def test_top_readings(list_of_lists):
     periods = ['week', 'month', 'year']
 
     for period in periods:
@@ -72,88 +71,85 @@ class Reddit(commands.Cog):
 
     @commands.command()
     async def get_reddit(self, ctx, mode='assorted'):
+        Reddit.reddit = await reddit_bot.reddit
 
         try:
-            period = sample(Reddit.timeframes, 1)[0]
-            category = sample(Reddit.sub_reddit_composite, 5)
-            await ctx.send(await readings_fetch(ctx,category, period, mode))
-        except Exception as E:
-            Reddit.timeframes = ['all', 'year', 'month']
-            Reddit.learning = ['learnprogramming',
-                                        'learnpython',
-                                        'learnlisp',
-                                        'learngolang',
-                                        'learnjava',
-                                        'cscareerquestions']
+            Reddit.reddit.init
+        except Exception as e:
+            Reddit.reddit.init = True
+            Reddit.reddit.timeframes = ['all', 'year', 'month']
+            Reddit.reddit.learning = ['learnprogramming',
+                                   'learnpython',
+                                   'learnlisp',
+                                   'learngolang',
+                                   'learnjava',
+                                   'cscareerquestions']
 
-            Reddit.ai = ['neuralnetworks',
-                                  'deeplearning',
-                                  'machinelearning',
-                                  'statistics']
+            Reddit.reddit.ai = ['neuralnetworks',
+                             'deeplearning',
+                             'machinelearning',
+                             'statistics']
 
-            Reddit.language = ['python',
-                                        'sql',
-                                        'julia',
-                                        'lisp',
-                                        'rlanguage',
-                                        'golang',
-                                        'rust',
-                                        'java',
-                                        'javascript',
-                                        'haskell',
-                                        'cpp',
-                                        'scala']
+            Reddit.reddit.language = ['python',
+                                   'sql',
+                                   'julia',
+                                   'lisp',
+                                   'rlanguage',
+                                   'golang',
+                                   'rust',
+                                   'java',
+                                   'javascript',
+                                   'haskell',
+                                   'cpp',
+                                   'scala']
 
-            Reddit.cstopics = ['programming',
-                                        'compsci',
-                                        'proceduralgeneration',
-                                        'crypto',
-                                        'demoscene'
+            Reddit.reddit.cstopics = ['programming',
+                                   'compsci',
+                                   'proceduralgeneration',
+                                   'crypto',
+                                   'demoscene'
+                                   ]
+
+            Reddit.reddit.industry = ['devops',
+                                   'technicaldebt',
+                                   'webdev',
+                                   'coding',
+                                   'datasets']
+
+            Reddit.reddit.entertainment = ['softwaregore',
+                                        'programmerhumor',
+                                        'ImaginaryFeels',
+                                        'awww',
+                                        'ultrahdwallpapers',
+                                        'wallpapers',
+                                        'minimalwallpaper',
+                                        'DnDGreentext',
+                                        'shitdwarffortresssays'
                                         ]
 
-            Reddit.industry = ['devops',
-                                        'technicaldebt',
-                                        'webdev',
-                                        'coding',
-                                        'datasets']
+            Reddit.reddit.categories = [
+                Reddit.reddit.learning,
+                Reddit.reddit.language,
+                Reddit.reddit.cstopics,
+                Reddit.reddit.ai,
+                Reddit.reddit.industry,
+                Reddit.reddit.entertainment
+            ]
 
-            Reddit.entertainment = ['softwaregore',
-                                             'programmerhumor',
-                                             'ImaginaryFeels',
-                                             'awww',
-                                             'ultrahdwallpapers',
-                                             'wallpapers',
-                                             'minimalwallpaper',
-                                             'DnDGreentext',
-                                             'shitdwarffortresssays'
-                                             ]
+            # initilization for horrible abuse of the python language
+            Reddit.reddit.sub_reddit_composite = []
 
-            Reddit.categories = [
-                                            Reddit.learning,
-                                            Reddit.language,
-                                            Reddit.cstopics,
-                                            Reddit.ai,
-                                            Reddit.industry,
-                                            Reddit.entertainment
-                                        ]
+            # horrible abuse of the python language. One line and it works. but feel free to make the below abuse/pythonic more readable.
+            [Reddit.reddit.sub_reddit_composite.extend(x) for x in Reddit.reddit.categories]
+        try:
+            period = sample(Reddit.reddit.timeframes, 1)[0]
 
-            #initilization for horrible abuse of the python language
-            Reddit.sub_reddit_composite = []
+            # category is a list of subreddit names to be concatenated after r/
+            category = sample(Reddit.reddit.sub_reddit_composite, 5)
+            print(readings_fetch(category, period, mode))
 
-            #horrible abuse of the python language. One line and it works. but feel free to make the below abuse/pythonic more readable.
-            [Reddit.sub_reddit_composite.extend(x) for x in Reddit.categories]
-
-
-            #category is a list of subreddit names to be concatenated after r/
-            category = sample(Reddit.sub_reddit_composite,7)
-
-
-            period = sample(Reddit.timeframes, 1)[0]
-            await ctx.send( await readings_fetch(ctx, category))
-
-    # @get_reddit.error
-    # async def get_reddit_error(self, ctx, error):
-    #     await ctx.send(f'Encountered an error: {error}')
+        except Exception as e:
+            print(e)
 
 
 def setup(bot):
