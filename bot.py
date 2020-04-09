f345f9774137de495b1468f4575a3f1edd8246dc
William Grigor <bjgrigor@gmail.com>
2020-04-09 16:01:18 -0500

    Removed old function

--- bot.py
+++ bot.py
@@ -17,9 +17,6 @@ def build_bot(prefix="!"):
 		print("Ready")
 		config = json.loads(open('assets/config.json', 'r').read())
 
-		#schedules the routine 'Kick unverified members' task
-		bot.loop.create_task(kick_unverified_task())
-
 		# Check if there are any new servers the bot does not have configs for
 		for server in bot.guilds:
 			if str(server.id) not in config:
@@ -59,35 +56,6 @@ def build_bot(prefix="!"):
 				reason = "Automated weekly kick due to not introducing yourself in the #if-you-are-new-click-here channel"
 				for member in unverified_members:
 					await guild.kick(member, reason=reason)
-
-
-	async def kick_unverified_task():
-		while True:
-			if datetime.today().weekday() !=6:
-				unverified_role = get(message.author.guild.roles, name="Unverified")
-				for member in ctx.guild.members:
-					if 'Unverified' in (role.name for role in member.roles):
-						await member.send(
-											"Automated Sunday Kick Warning: You will be kicked end of day Sunday if you do not " \
-											"introduce yourself in #if-you-are-new-click here within the Coding Community server."
-										)
-			else:
-				for member in ctx.guild.members:
-					if 'Unverified' in (role.name for role in member.roles):
-						await member.send(
-											"Automated Sunday Kick Warning: You will be kicked in 5 minutes if you do not " \
-											"introduce yourself in #if-you-are-new-click here within the Coding Community server."
-										)
-
-				await asyncio.sleep(300)
-
-				reason = "Automated weekly kick due to not introducing yourself in the #if-you-are-new-click-here channel"
-				for member in ctx.guild.members:
-					if 'Unverified' in (role.name for role in member.roles):
-						await ctx.message.guild.kick(member, reason=reason)
-
-				await asyncio.sleep(86400)
->>>>>>> Stashed changes
 
 	@bot.event
 	async def on_message(message):
