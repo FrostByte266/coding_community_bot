e574e8897ad099a254a73593bec3700db4e034e5
William Grigor <bjgrigor@gmail.com>
2020-04-10 20:31:33 -0500

    Auto reddit channel management

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -10,9 +10,11 @@ def load_reddit_conf():
     config = json.loads(open('assets/config.json', 'r').read())
     return config["reddit_config"]
 
-def __init__(self,bot):
+
+def __init__(self, bot):
     self.bot = bot
     self.reddit_config = self.bot.reddit_config
+
 
 def reddit_bot():
     try:
@@ -30,6 +32,7 @@ def reddit_bot():
         reddit_bot.reddit = reddit
         return reddit
 
+
 async def topinxperiod(subreddit, period='year', return_quantity=3):
     try:
         if return_quantity > 7:
@@ -37,7 +40,8 @@ async def topinxperiod(subreddit, period='year', return_quantity=3):
 
         reddit = reddit_bot()
         # relevant documentation https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html
-        content = [submission.url for submission in reddit.subreddit(subreddit).top(period,limit=return_quantity)]
+        content = [submission.url for submission in reddit.subreddit(
+            subreddit).top(period, limit=return_quantity)]
         return content
     except Exception as e:
         print(str(e))
@@ -74,10 +78,12 @@ async def test_top_readings(list_of_lists):
             top_links_in_period.extend(topinxperiod(subreddit, period))
         print(len(''.join(top_links_in_period)))
 
+
 class Reddit(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.config_full = json.loads(open('assets/config.json', 'r').read())
 
     @commands.command()
     async def get_reddit(self, ctx, mode='assorted'):
@@ -91,7 +97,7 @@ class Reddit(commands.Cog):
                                    'learnpython',
                                    'learngolang']
 
-            Reddit.ai = ['neuralnetworks','statistics']
+            Reddit.ai = ['neuralnetworks', 'statistics']
 
             Reddit.language = [
                                 'python',
@@ -144,29 +150,29 @@ class Reddit(commands.Cog):
 
             # category is a list of subreddit names to be concatenated after r/
             category = sample(Reddit.sub_reddit_composite, 5)
-            await ctx.send(await readings_fetch(ctx,category, period=period, mode=mode))
+            await ctx.send(await readings_fetch(ctx, category, period=period, mode=mode))
 
         except Exception as e:
             print(e)
 
-	@commands.command()
-	@commands.has_permissions(manage_guild=True)
-	async def reddit(self, ctx, state: bool):
-		"""Enable or disable the reddit system"""
-		config = self.config_full[str(ctx.message.guild.id)]
-		if state is True and config["reddit"] is None:
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def reddit(self, ctx, state: bool):
+        """Enable or disable the reddit system"""
+        config = self.config_full[str(ctx.message.guild.id)]
+        if state is True and config["reddit_channel"] is None:
             permission_overrides = {
-                ctx.guild.default_role: PermissionOverwrite(send_messages=False)
+                ctx.guild.default_role: PermissionOverwrite(send_messages=False),
                 ctx.guild.me: PermissionOverwrite(send_messages=True)
             }
-			channel = await ctx.message.guild.create_channel("reddit-feed", overwrites=permission_overrides)
-			config.update(reddit_channel = channel.id)
-			json.dump(self.config_full, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
-		elif state is False and config["reddit_channel"] is not None:
-			channel = bot.get_channel(config["reddit_channel"])
-			await channel.delete()
-			config.update(reddit_channel=None)
-			json.dump(self.config_full, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
+            channel = await ctx.message.guild.create_text_channel("reddit-feed", overwrites=permission_overrides)
+            config.update(reddit_channel = channel.id)
+            json.dump(self.config_full, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
+        elif state is False and config["reddit_channel"] is not None:
+            channel = self.bot.get_channel(config["reddit_channel"])
+            await channel.delete()
+            config.update(reddit_channel=None)
+            json.dump(self.config_full, open('assets/config.json', 'w'), indent=2, separators=(',', ': '))
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
