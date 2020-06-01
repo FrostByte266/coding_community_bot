400c29809daf03d6253a3c719b8c9561345d071d
William Grigor <bjgrigor@gmail.com>
2020-05-31 20:58:44 -0500

    List unverified sorts alphabetically

--- cogs/admin.py
+++ cogs/admin.py
@@ -84,7 +84,7 @@ class Admin(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     async def list_unverified(self, ctx):
         unverfied_role = get(ctx.guild.roles, name='Unverified')
-        list_of_members = [f'{member.name} (ID: {member.id})' for member in unverfied_role.members]
+        list_of_members = [f'{member.name} (ID: {member.id})' for member in unverfied_role.members.sort()]
         newline_separated_list = '\n'.join(list_of_members)
         length_of_list = len(newline_separated_list)
         if length_of_list > 2000:
